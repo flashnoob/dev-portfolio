824e5d2a470111299e3835d653d0f61f892d7e4a
Sachin kotian <sachinKotian007@yahoo.com>
2021-03-02 21:51:22 +0530

    Changed AOS config

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,8 @@ import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
 AOS.init({
   anchorPlacement: 'top-left',
-  duration: 1000
+  duration: 1000,
+  once:true
 });
 
 // Add your javascript here
