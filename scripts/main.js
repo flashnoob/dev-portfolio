38ad445723f6890469b70119134149241346c374
Sachin kotian <sachinKotian007@yahoo.com>
2021-03-02 22:01:03 +0530

    Added 🌙 Dark mode in app

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 // Animations
 import AOS from 'aos';
+import Darkmode from 'darkmode-js';
+
 import 'aos/dist/aos.css'; // You can also use <link> for styles
 AOS.init({
   anchorPlacement: 'top-left',
@@ -9,50 +11,17 @@ AOS.init({
 
 // Add your javascript here
 
-
-// you can use app's unique identifier here
-const LOCAL_STORAGE_KEY = "toggle-bootstrap-theme";
-
-const LOCAL_META_DATA = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-
-// you can change this url as needed
-const DARK_THEME_PATH = "https://bootswatch.com/4/cyborg/bootstrap.min.css";
-
-const DARK_STYLE_LINK = document.getElementById("dark-theme-style");
-const THEME_TOGGLER = document.getElementById("theme-toggler");
-
-let isDark = LOCAL_META_DATA && LOCAL_META_DATA.isDark;
-
-// check if user has already selected dark theme earlier
-if (isDark) {
-  enableDarkTheme();
-} else {
-  disableDarkTheme();
+ 
+const options = {
+  bottom: '10px', // default: '32px'
+  right:'10px',
+  time: '0.5s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#fff',  // default: '#fff'
+  buttonColorDark: '#100f2c',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies:true, // default: true,
+  label: '🌓', // default: ''
+  autoMatchOsTheme: true // default: true
 }
-
-
-/**
- * Apart from toggling themes, this will also store user's theme preference in local storage.
- * So when user visits next time, we can load the same theme.
- *
- */
-function toggleTheme() {
-  isDark = !isDark;
-  if (isDark) {
-    enableDarkTheme();
-  } else {
-    disableDarkTheme();
-  }
-  const META = { isDark };
-  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(META));
-}
-
-function enableDarkTheme() {
-  DARK_STYLE_LINK.setAttribute("href", DARK_THEME_PATH);
-  THEME_TOGGLER.innerHTML = "🌙 Dark";
-}
-
-function disableDarkTheme() {
-  DARK_STYLE_LINK.setAttribute("href", "");
-  THEME_TOGGLER.innerHTML = "🌞 Light";
-}
+new Darkmode(options).showWidget();
