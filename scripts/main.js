f7c8c5ceb0838d00534d33a6e202388bf971bda6
Sachin kotian <sachinKotian007@yahoo.com>
2021-03-06 17:24:01 +0530

    Fixed dark mode flash

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,5 @@
 // Animations
 import AOS from 'aos';
-import Darkmode from 'darkmode-js';
 
 import 'aos/dist/aos.css'; // You can also use <link> for styles
 AOS.init({
@@ -12,16 +11,3 @@ AOS.init({
 // Add your javascript here
 
  
-const options = {
-  bottom: '10px', // default: '32px'
-  right:'10px',
-  time: '0.5s', // default: '0.3s'
-  mixColor: '#fff', // default: '#fff'
-  backgroundColor: '#fff',  // default: '#fff'
-  buttonColorDark: '#100f2c',  // default: '#100f2c'
-  buttonColorLight: '#fff', // default: '#fff'
-  saveInCookies:true, // default: true,
-  label: '🌓', // default: ''
-  autoMatchOsTheme: true // default: true
-}
-new Darkmode(options).showWidget();
