77daa504ed4f64868c758818a05a25fab6999946
Sachin kotian <sachinKotian007@yahoo.com>
2021-03-06 17:50:12 +0530

    Added service worker

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,22 @@
 // Animations
 import AOS from 'aos';
+import Darkmode from 'darkmode-js';
 
 import 'aos/dist/aos.css'; // You can also use <link> for styles
+
+const options = {
+  bottom: '10px', // default: '32px'
+  right:'10px',
+  time: '0.5s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#fff',  // default: '#fff'
+  buttonColorDark: '#100f2c',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies:true, // default: true,
+  label: '🌓', // default: ''
+  autoMatchOsTheme: true // default: true
+}
+new Darkmode(options).showWidget();
 AOS.init({
   anchorPlacement: 'top-left',
   duration: 1000,
@@ -10,4 +25,8 @@ AOS.init({
 
 // Add your javascript here
 
- 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('../pwabuilder-sw/pwabuilder-sw.js');
+  })
+}
